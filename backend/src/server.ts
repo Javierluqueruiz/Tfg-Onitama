ff2927df84d159a87871eff43dd572f8496f0d42
Javier Luque <javluqrui@alum.us.es>
2026-04-10 20:33:10 +0200

    feat: smoke test para probar el movimiento de las piezas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import http from 'http';
 import { Server, Socket } from 'socket.io'
 import { GameEngine } from './game/GameEngine';
+import { MovementManager } from './game/MovementManager';
 
 const app = express();
 app.use(cors());
@@ -38,5 +39,20 @@ server.listen(PORT, () =>{
 console.log("\n====== NUEVA PARTIDA INICIADA ====\n");
 
 const gameState = GameEngine.createNewGame('room1');
-console.log("Estado inicial del juego:");
-console.log(JSON.stringify(gameState, null, 2));
+//console.log("Estado inicial del juego:");
+//console.log(JSON.stringify(gameState, null, 2));
+
+console.log("\n=== TABLERO INICIAL ===");
+console.table(gameState.board);
+
+console.log("\n=== EJECUTANDO MOVIMIENTOS DE PRUEBA ===");
+console.log("Moviendo la pieza de la esquina superior izquierda (0,0) a la casilla inferior (0,1)");
+
+const newBoard = MovementManager.movePiece(gameState.board, { x: 0, y: 0 }, { x: 0, y: 1 });
+
+console.log("\n=== TABLERO DESPUÉS DEL MOVIMIENTO ===");
+console.table(newBoard);
+
+console.log("\n=== VERIFICANDO QUE EL TABLERO ORIGINAL NO SE MODIFICÓ ===");
+console.log("\n=== TABLERO ORIGINAL ===");
+console.table(gameState.board);
