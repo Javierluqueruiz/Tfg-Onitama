dde4bcc58d1862f75cf12ee416ae0daa1d8babc2
Javier Luque <javluqrui@alum.us.es>
2026-04-11 20:52:47 +0200

    feat: Test de la captura

--- backend/src/game/MovementManager.test.ts
+++ backend/src/game/MovementManager.test.ts
@@ -4,7 +4,7 @@ import { GameEngine } from "./GameEngine";
 import { Board } from "../../../shared/types";
 import { BoardGenerator } from "./BoardGenerator";
 
-describe('FEAT-03: MovementManager', () => {
+describe('FEAT-03 y FEAT-04: MovementManager', () => {
 
     let initialBoard: Board;
     beforeAll(() => {
@@ -17,7 +17,7 @@ describe('FEAT-03: MovementManager', () => {
         const from = { x: 0, y: 0 }; //Posición inicial de una pieza roja
         const to = { x: 0, y: 1 }; //Posición destino vacía
         
-        const newBoard = MovementManager.movePiece(initialBoard, from, to);
+        const { newBoard, capturedPiece } = MovementManager.movePiece(initialBoard, from, to);
 
         expect(newBoard[to.y][to.x]).toEqual(initialBoard[from.y][from.x]); //La pieza se mueve a la nueva posición
         expect(newBoard[from.y][from.x]).toBeNull(); //La posición original queda vacía
@@ -26,14 +26,15 @@ describe('FEAT-03: MovementManager', () => {
     it('Debe lanzar un error si no hay pieza en la posición de origen', () => {
         const from = { x: 2, y: 2 }; //Posición vacía
         const to = { x: 2, y: 3 }; //Posición destino vacía
-        expect(() => MovementManager.movePiece(initialBoard, from, to)).toThrowError(`[FEAT-03] No hay pieza en la posición de origen (${from.x}, ${from.y})`);
+        expect(() => MovementManager.movePiece(initialBoard, from, to))
+            .toThrowError(`[FEAT-03] No hay pieza en la posición de origen (${from.x}, ${from.y})`);
     });
 
     it('Debe retornar un nuevo tablero sin modificar el original', () => {
         const from = { x: 0, y: 0 };
         const to = { x: 0, y: 1 };
 
-        const newBoard = MovementManager.movePiece(initialBoard, from, to);
+        const { newBoard } = MovementManager.movePiece(initialBoard, from, to);
         //El tablero original no debe modificarse
         expect(initialBoard[from.y][from.x]).not.toBeNull();
         expect(initialBoard[to.y][to.x]).toBeNull();
@@ -44,5 +45,21 @@ describe('FEAT-03: MovementManager', () => {
             .toThrowError(`[FEAT-03] La posición de origen (-1, 0) está fuera de los límites del tablero`);
         expect(() => MovementManager.movePiece(initialBoard, { x: 0, y: 0 }, { x: 5, y: 1 }))
             .toThrowError(`[FEAT-03] La posición de destino (5, 1) está fuera de los límites del tablero`);
+    });
+
+    it('FEAT-04: Debe capturar la pieza en la posición destino si existe', () => {
+        const from = {x: 0, y: 0};
+        const to = {x: 0, y: 4}; 
+
+        const targetPiece = initialBoard[to.y][to.x]; 
+
+        const { newBoard, capturedPiece } = MovementManager.movePiece(initialBoard, from, to);
+
+        expect(newBoard[to.y][to.x]).toEqual(initialBoard[from.y][from.x]); 
+
+        expect(capturedPiece).not.toBeNull();
+        expect(capturedPiece).toEqual(targetPiece);
+        expect(capturedPiece?.color).toEqual(targetPiece?.color);
+        expect(capturedPiece?.type).toEqual(targetPiece?.type);
     });
 })
