582c166cdefff3becaaccbcf4c7deeaeb5132bd8
Javier Luque <javluqrui@alum.us.es>
2026-08-14 16:31:13 +0200

    feat: primera versión de la representación de las cartas

--- frontend/src/components/game/GameScreen.tsx
+++ frontend/src/components/game/GameScreen.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import type { GameState } from '../../../../shared';
 import { BoardView } from './BoardView';
+import { CardView } from './CardView';
 
 interface GameScreenProps {
     gameState: GameState;
 }
 
 export const GameScreen: React.FC<GameScreenProps> = ({ gameState })  => {
+    const {board, currentTurn} = gameState;
+    const redCards = gameState.cards.red;
+    const blueCards = gameState.cards.blue;
+    const neutralCard = gameState.cards.neutral;
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px', gap: '20px' }}>
@@ -14,11 +19,35 @@ export const GameScreen: React.FC<GameScreenProps> = ({ gameState })  => {
                 <h2>Sala de Juego</h2>
                 <h3 style={{ margin: 0 }}>
                     Turno actual: <span style={{ color: gameState.currentTurn === 'red' ? 'red' : 'blue' }}>
-                        {gameState.currentTurn}
+                        {currentTurn}
                     </span>
                 </h3>
             </div>
-            <BoardView board={gameState.board} />
+
+            {/* Zona del Jugador Rival */}
+            <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
+                {blueCards.map((card, index) => (
+                    <CardView key={`blue-card-${index}`} card={card} faction="blue" />
+                ))}
+            </div>
+
+            <BoardView board={board} />
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px' }}>
+                    <span style={{ fontWeight: 'bold', color: '#7f8c8d', fontSize: '14px', textTransform: 'uppercase' }}>
+                        Mesa (Siguiente)
+                    </span>
+                    {neutralCard && (
+                        <CardView card={neutralCard} faction="neutral" />
+                    )}
+                </div>
+
+
+            {/* Zona del Jugador Local */}
+            <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
+                {redCards.map((card, index) => (
+                    <CardView key={`red-card-${index}`} card={card} faction="red" />
+                ))}
+            </div>
         </div>
 
     );
